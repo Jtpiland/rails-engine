47e9861ddb297375b7f616b68a62a4abafa119a5
Jacob Piland <piland.jacob@gmail.com>
2021-09-03 17:35:42 -0500

    Add model factories config

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
-  facotry :invoice_item do
+  factory :invoice_item do
     quantity { Faker::Number.decimal_part(digits: 2) }
     sequence(:unit_price) { |n| n +150 }
     status { Faker::Number.between(from: 0, to: 2) }
     invoice
     item
+  end
 end
--- spec/factories/merchant.rb
+++ spec/factories/merchant.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :customer do
+  factory :merchant do
     name { Faker::Company.name }
   end
 end
