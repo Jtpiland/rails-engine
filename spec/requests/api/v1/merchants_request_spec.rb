5388e590478e61d9a20e18b918b8bcf16904033b
Jacob Piland <piland.jacob@gmail.com>
2021-09-03 23:58:21 -0500

    Add merchant serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it "sends a list of merchants" do
-    create_list(:merchant, 20)
+    create_list(:merchant, 30)
 
-    get '/api/v1/merchants'
+    get '/api/v1/merchants', params: { page: 1 } #?page
 
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(20)
+    expect(merchants[:data].count).to eq(20)
 
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
 end
