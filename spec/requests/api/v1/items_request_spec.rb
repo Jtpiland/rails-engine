6b6c225a6333a24f53d2862dc7b68a7467df980c
Jacob Piland <piland.jacob@gmail.com>
2021-09-04 18:05:34 -0500

    Refactor Items request spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,28 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe "Items API" do
-  it 'sends a list of items' do
-    create_list(:item, 30)
+  describe 'GET /api/vi/items' do
+    it 'sends a list of items' do
+      create_list(:item, 30)
 
-    get '/api/v1/items', params: { page: 1 }
+      get '/api/v1/items', params: { page: 1 }
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    items = JSON.parse(response.body, symbolize_names: true)
+      items = JSON.parse(response.body, symbolize_names: true)
 
-    expect(items[:data].count).to eq(20)
+      expect(items[:data].count).to eq(20)
 
-    items[:data].each do |item|
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_a(String)
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_a(String)
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_a(String)
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_a(Float)
-      expect(item[:attributes]).to have_key(:merchant_id)
-      expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      end
+    end
+  end
+
+  describe 'POST /api/v1/items' do
+    let(:valid_attributes) { { name: 'Item', description: 'Description', unit_price: 150.0, merchant_id: Merchant.first.id } }
+
+    before :each do
+      create_list(:merchant, 2)
+      post '/api/v1/items', params: valid_attributes
+    end
+
+    describe 'create and delete' do
+
+      it 'can create an item' do
+        expect(response).to be_successful
+
+        item = JSON.parse(response.body, symbolize_names: true)
+        require "pry"; binding.pry
+      end
+
+      it 'can delete an item' do
+        delete "/api/v1/items/#{Item.first.id}"
+
+        expect(response).to be_successful
+      end
     end
   end
 end
