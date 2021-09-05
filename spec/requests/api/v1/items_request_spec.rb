566e3b69b35de8df0238e8e47fc024d0f66b64d7
Jacob Piland <piland.jacob@gmail.com>
2021-09-05 09:22:50 -0500

    Refactor create item test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -42,7 +42,17 @@ RSpec.describe "Items API" do
         expect(response).to be_successful
 
         item = JSON.parse(response.body, symbolize_names: true)
-        require "pry"; binding.pry
+
+        expect(item[:data]).to have_key(:id)
+        expect(item[:data][:id]).to be_a(String)
+        expect(item[:data][:attributes]).to have_key(:name)
+        expect(item[:data][:attributes][:name]).to be_a(String)
+        expect(item[:data][:attributes]).to have_key(:description)
+        expect(item[:data][:attributes][:description]).to be_a(String)
+        expect(item[:data][:attributes]).to have_key(:unit_price)
+        expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+        expect(item[:data][:attributes]).to have_key(:merchant_id)
+        expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
       end
 
       it 'can delete an item' do
