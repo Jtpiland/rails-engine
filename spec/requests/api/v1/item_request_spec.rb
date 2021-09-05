d4e8f62e4a54367c4834bbc326a410d5a9487303
Jacob Piland <piland.jacob@gmail.com>
2021-09-05 09:23:46 -0500

    Add update item spec

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -1,25 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Item Show API" do
-  it 'sends information about a particular item' do
+  before :each do
     create_list(:item, 2)
+  end
 
-    get "/api/v1/items/#{Item.first.id}"
+  describe 'GET /api/vi/items/#{item_id}'
+    it 'sends information about a particular item' do
 
-    expect(response).to be_successful
+      get "/api/v1/items/#{Item.first.id}"
 
-    item = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
 
-    expect(item[:data].count).to eq(3)
-    expect(item[:data]).to have_key(:id)
-    expect(item[:data][:id]).to be_a(String)
-    expect(item[:data][:attributes]).to have_key(:name)
-    expect(item[:data][:attributes][:name]).to be_a(String)
-    expect(item[:data][:attributes]).to have_key(:description)
-    expect(item[:data][:attributes][:description]).to be_a(String)
-    expect(item[:data][:attributes]).to have_key(:unit_price)
-    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
-    expect(item[:data][:attributes]).to have_key(:merchant_id)
-    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item[:data].count).to eq(3)
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+      expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+    end
+
+  describe 'PUT /api/v1/items/#{item_id}' do
+    let(:valid_attributes) { { name: 'Updated Name'} }
+
+    it 'can update information about a particular item' do
+
+      put "/api/v1/items/#{Item.first.id}", params: valid_attributes
+
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item[:data][:attributes][:name]).to eq('Updated Name')
+    end
   end
 end
