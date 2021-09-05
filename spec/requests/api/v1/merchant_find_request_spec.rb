621a31f764995440ac09181220a3d02d61164a6c
Jacob Piland <piland.jacob@gmail.com>
2021-09-05 11:01:41 -0500

    Add merchant find request spec

---
+++ spec/requests/api/v1/merchant_find_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Find API' do
+  before :each do
+    create_list(:merchant, 2)
+  end
+
+  describe 'GET /api/v1/merchants/find' do
+    it 'can find a single merchant based on a search' do
+
+      get '/api/v1/merchants/find'
+
+      expect(response).to be_successful
+    end
+  end
+end
