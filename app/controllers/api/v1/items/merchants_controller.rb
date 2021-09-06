7b0664294166c7816d195f900b4bc6a0c454e1b8
Jacob Piland <piland.jacob@gmail.com>
2021-09-05 19:36:05 -0500

    Refactor item/merchant index method

--- app/controllers/api/v1/items/merchants_controller.rb
+++ app/controllers/api/v1/items/merchants_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Items::MerchantsController < ApplicationController
 
   def index
     item = Item.find(params[:item_id])
-    merchant = Merchant.where('id = ?', item.merchant_id)
+    merchant = Merchant.find(item.merchant_id)
 
     render json: MerchantSerializer.new(merchant)
   end
