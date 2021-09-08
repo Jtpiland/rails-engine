e88f7c2a1b3a185fdfe26f2e9b53daf56d17a462
Jacob Piland <piland.jacob@gmail.com>
2021-09-08 18:20:08 -0500

    Add ::top_items_by_revenue to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,13 @@ class Item < ApplicationRecord
   validates_presence_of :description
   validates_presence_of :unit_price
   validates_presence_of :merchant_id
+
+  def self.top_items_by_revenue(params_quantity)
+    joins(invoice_items: { invoice: :transactions })
+    .where("invoices.status='shipped' AND transactions.result='success'")
+    .group('items.id')
+    .select("items.*, SUM(invoice_items.quantity *  invoice_items.unit_price) AS revenue")
+    .order('revenue DESC')
+    .limit(params_quantity)
+  end
 end
