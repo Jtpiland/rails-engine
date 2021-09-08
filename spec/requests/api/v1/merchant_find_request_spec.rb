a4d4dc26e6e92f0ef1ccb6b81ae21bf978973b5d
Jacob Piland <piland.jacob@gmail.com>
2021-09-07 19:09:53 -0500

    Refactor Merchant Find spec

--- spec/requests/api/v1/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchant_find_request_spec.rb
@@ -2,15 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Find API' do
   before :each do
-    create_list(:merchant, 2)
+    create(:merchant, name: 'Kevin')
+    create(:merchant, name: 'Todd')
   end
 
   describe 'GET /api/v1/merchants/find' do
-    it 'can find a single merchant based on a search' do
+    it 'can find a single merchant based on a search fragment' do
 
-      get "/api/v1/merchants/find"
+      get "/api/v1/merchants/find?name=Kev"
 
       expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:attributes][:name]).to eq('Kevin')
+    end
+
+    it 'sends an error message if the no merchant matches the search fragment' do
+
+      get "/api/v1/merchants/find?name=Dog"
+
+      expect(response).to be_successful
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result[:data][:status]).to eq(200)
     end
   end
 end
+
+#Tests that show case insensitive search?
