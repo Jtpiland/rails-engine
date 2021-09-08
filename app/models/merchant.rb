7ff8295787c7a18652bb5f36e55d4fa6770992a7
Jacob Piland <piland.jacob@gmail.com>
2021-09-08 18:53:12 -0500

    Add #top_merchants_by_revenue to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,12 @@ class Merchant < ApplicationRecord
 
   validates_presence_of :name
 
+  def self.top_merchants_by_revenue(params_quantity)
+    joins(items: { invoice_items: { invoice: :transactions }})
+    .where("invoices.status='shipped' AND transactions.result='success'")
+    .group('merchants.id')
+    .select("merchants.*, SUM(invoice_items.quantity *  invoice_items.unit_price) AS revenue")
+    .order('revenue DESC')
+    .limit(params_quantity)
+  end
 end
