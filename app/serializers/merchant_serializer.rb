a756c6db07a65ebf157fc622cb7be7f0231d95ba
Jacob Piland <piland.jacob@gmail.com>
2021-09-09 18:59:42 -0500

    Remove commented out method

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,13 +1,4 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
-
-  # def self.format_merchants(merchants)
-  #   merchants.map do |merchant|
-  #     { data: [
-  #       id: merchant.id,
-  #       name: merchant.name]
-  #     }
-  #   end
-  # end
 end
